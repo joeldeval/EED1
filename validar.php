974e5e0ff4befc6e209b26f3d951e52c9c4dd2f2
JoelValdivia <jooelvaldivi@gmail.com>
2013-10-31 12:52:42 -0600

    inicio

--- validar.php
+++ validar.php
@@ -7,14 +7,15 @@
     
 <?php
    
-    $Usuario = $_POST['usuario'];
-    $Contra = $_POST['contra'];
+    $Usuario = $_POST['txtusuario'];
+    $Contra = $_POST['txtcontra'];
 if( ($Usuario == "joel" && $Contra == "joel")){
 	session_start();
-	$_SESSION["codigo"] = $Codigo;
+	$_SESSION["usuario"] = $Usuario;
 	header("Location: inicio.php");
       }else{
 	header("Location: principal.php");
+  echo "INCORRECTO";
 	}
 ?>
  
